01c453034ea3a83b9752a89e574e61b004ddd314
daniil <dannydendi@gmail.com>
2023-08-08 02:36:22 +0300

    timezone

--- src/main/java/com/example/TimeServlet.java
+++ src/main/java/com/example/TimeServlet.java
@@ -1,36 +1,89 @@
 package com.example;
 
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 import java.io.IOException;
+import java.time.DateTimeException;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.Date;
 
 @WebServlet("/time")
 public class TimeServlet extends HttpServlet {
+    private transient TemplateEngine templateEngine;
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        String timezone = req.getParameter("timezone");
+    public void init(ServletConfig config) throws ServletException {
+        templateEngine = createTemplateEngine();
+    }
 
-        if (timezone == null || timezone.isEmpty()) {
-            resp.getWriter().write(getDate("UTC"));
+    private TemplateEngine createTemplateEngine() {
+        TemplateEngine engine = new TemplateEngine();
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+        resolver.setPrefix("templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+        resolver.setOrder(engine.getTemplateResolvers().size());
+        resolver.setCacheable(false);
+        engine.addTemplateResolver(resolver);
+        return engine;
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        String timezoneParam = req.getParameter("timezone");
+        String formattedDate = "";
+
+        if (timezoneParam == null || timezoneParam.isEmpty() || !isValidTimezone(timezoneParam)) {
+            Cookie[] cookies = req.getCookies();
+            if (cookies != null && cookies.length > 0) {
+                formattedDate = getDateFromCookie(cookies);
+            }
         } else {
-            String date = getDate(timezone);
-            resp.getWriter().write(date);
+            resp.addCookie(new Cookie("Date", timezoneParam));
+            formattedDate = getDate(timezoneParam);
+        }
+
+        Context context = new Context(req.getLocale());
+        context.setVariable("date", formattedDate);
+        templateEngine.process("text", context, resp.getWriter());
+    }
+
+    private boolean isValidTimezone(String timezone) {
+        try {
+            ZoneId.of(timezone);
+            return true;
+        } catch (DateTimeException e) {
+            return false;
         }
     }
 
-    public static String getDate(String timeZone) {
+    private String getDateFromCookie(Cookie[] cookies) {
+        String timezoneCookie = Arrays.stream(cookies)
+                .filter(cookie -> cookie.getName().equals("Date"))
+                .findFirst()
+                .map(Cookie::getValue)
+                .orElse("");
+        return !timezoneCookie.isEmpty() ? getDate(timezoneCookie) : "";
+    }
+
+    private String getDate(String param) {
         Date actualDate = new Date();
         DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("uuuu-MM-dd HH:mm:ss z")
-                .withZone(ZoneId.of(timeZone));
+                .withZone(ZoneId.of(param));
         return dateFormat.format(actualDate.toInstant());
     }
 }
