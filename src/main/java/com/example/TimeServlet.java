e7244577577740a53531923b17db7daa63c1c0e3
daniil <dannydendi@gmail.com>
2023-08-09 20:52:47 +0300

    timezone

--- src/main/java/com/example/TimeServlet.java
+++ src/main/java/com/example/TimeServlet.java
@@ -1,7 +1,6 @@
 package com.example;
 
 import jakarta.servlet.ServletConfig;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
@@ -9,81 +8,70 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
 
 import java.io.IOException;
-import java.time.DateTimeException;
+import java.net.URLEncoder;
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.Date;
+import java.util.Map;
+import java.util.Objects;
 
 @WebServlet("/time")
 public class TimeServlet extends HttpServlet {
-    private transient TemplateEngine templateEngine;
+    private transient TemplateEngine engine;
 
     @Override
-    public void init(ServletConfig config) throws ServletException {
-        templateEngine = createTemplateEngine();
-    }
-
-    private TemplateEngine createTemplateEngine() {
-        TemplateEngine engine = new TemplateEngine();
-        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
-        resolver.setPrefix("templates/");
+    public void init(ServletConfig config) {
+        engine = new TemplateEngine();
+        FileTemplateResolver resolver = new FileTemplateResolver();
+        resolver.setPrefix(Objects.requireNonNull(getClass().getClassLoader().getResource("templates")).getPath());
         resolver.setSuffix(".html");
         resolver.setTemplateMode("HTML5");
         resolver.setOrder(engine.getTemplateResolvers().size());
         resolver.setCacheable(false);
         engine.addTemplateResolver(resolver);
-        return engine;
     }
-
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String date = "";
+        String timezone = "timezone";
         resp.setContentType("text/html");
+        String parameter = req.getParameter(timezone);
 
-        String timezoneParam = req.getParameter("timezone");
-        String formattedDate = "";
+        String lastTimezone = null;
+        Cookie[] cookies = req.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("lastTimezone")) {
+                    lastTimezone = cookie.getValue();
+                    break;
+                }
+            }
+        }
 
-        if (timezoneParam == null || timezoneParam.isEmpty() || !isValidTimezone(timezoneParam)) {
-            Cookie[] cookies = req.getCookies();
-            if (cookies != null && cookies.length > 0) {
-                formattedDate = getDateFromCookie(cookies);
+        if (parameter == null || parameter.isEmpty()) {
+            if (lastTimezone != null) {
+                date = getDate(lastTimezone);
+            } else {
+                date = getDate("UTC");
             }
         } else {
-            resp.addCookie(new Cookie("Date", timezoneParam));
-            formattedDate = getDate(timezoneParam);
+            String encodedParameter = URLEncoder.encode(parameter, "UTF-8");
+            resp.addCookie(new Cookie("lastTimezone", encodedParameter));
+            date = getDate(parameter);
         }
 
-        Context context = new Context(req.getLocale());
-        context.setVariable("date", formattedDate);
-        templateEngine.process("text", context, resp.getWriter());
+
+        Context context = new Context(req.getLocale(), Map.of("date", date));
+        engine.process("result", context, resp.getWriter());
     }
 
-    private boolean isValidTimezone(String timezone) {
-        try {
-            ZoneId.of(timezone);
-            return true;
-        } catch (DateTimeException e) {
-            return false;
-        }
-    }
-
-    private String getDateFromCookie(Cookie[] cookies) {
-        String timezoneCookie = Arrays.stream(cookies)
-                .filter(cookie -> cookie.getName().equals("Date"))
-                .findFirst()
-                .map(Cookie::getValue)
-                .orElse("");
-        return !timezoneCookie.isEmpty() ? getDate(timezoneCookie) : "";
-    }
-
-    private String getDate(String param) {
-        Date actualDate = new Date();
-        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("uuuu-MM-dd HH:mm:ss z")
-                .withZone(ZoneId.of(param));
-        return dateFormat.format(actualDate.toInstant());
+    public String getDate(String param) {
+        ZonedDateTime actualDateTime = ZonedDateTime.now(ZoneId.of(param));
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("uuuu-MM-dd HH:mm:ss z");
+        return dateFormat.format(actualDateTime);
     }
 }
--- src/main/java/com/example/TimezoneValidateFilter.java
+++ src/main/java/com/example/TimezoneValidateFilter.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.time.zone.ZoneRulesException;
 
 @WebFilter("/time")
-public class TimezoneValidateFilter implements Filter {
+public class TimezoneValidateFilter implements jakarta.servlet.Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -16,8 +16,7 @@ public class TimezoneValidateFilter implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
-            throws ServletException, IOException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws ServletException, IOException {
         String timezone = servletRequest.getParameter("timezone");
         if (timezone == null || timezone.isEmpty()) {
             filterChain.doFilter(servletRequest, servletResponse);
@@ -26,8 +25,9 @@ public class TimezoneValidateFilter implements Filter {
                 filterChain.doFilter(servletRequest, servletResponse);
             } catch (ZoneRulesException e) {
                 HttpServletResponse response = (HttpServletResponse) servletResponse;
-                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-                response.getWriter().write("Invalid timezone! STATUS CODE = " + response.getStatus());
+
+                response.setStatus(400);
+                response.getWriter().write("Invalid timezone!" + " STATUS CODE = " + response.getStatus());
             }
         }
     }
