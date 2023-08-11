215edb84ed4a3b32c8ce715bfb82315c514d02ee
daniil <dannydendi@gmail.com>
2023-08-11 03:04:09 +0300

    timezone

--- src/main/java/com/example/TimeServlet.java
+++ src/main/java/com/example/TimeServlet.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import jakarta.servlet.ServletConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
@@ -11,22 +10,21 @@ import org.thymeleaf.context.Context;
 import org.thymeleaf.templateresolver.FileTemplateResolver;
 
 import java.io.IOException;
-import java.net.URLEncoder;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.Map;
-import java.util.Objects;
 
 @WebServlet("/time")
 public class TimeServlet extends HttpServlet {
     private transient TemplateEngine engine;
 
     @Override
-    public void init(ServletConfig config) {
+    public void init() {
         engine = new TemplateEngine();
         FileTemplateResolver resolver = new FileTemplateResolver();
-        resolver.setPrefix(Objects.requireNonNull(getClass().getClassLoader().getResource("templates")).getPath());
+        resolver.setPrefix(getClass().getClassLoader().getResource("templates").getPath());
         resolver.setSuffix(".html");
         resolver.setTemplateMode("HTML5");
         resolver.setOrder(engine.getTemplateResolvers().size());
@@ -34,48 +32,47 @@ public class TimeServlet extends HttpServlet {
         engine.addTemplateResolver(resolver);
     }
 
-    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        String date = "";
-        String timezone = "timezone";
         resp.setContentType("text/html");
-        String parameter = req.getParameter(timezone);
+        String timezoneParam = req.getParameter("timezone");
 
-        String lastTimezone = null;
-        Cookie[] cookies = req.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("lastTimezone")) {
-                    lastTimezone = cookie.getValue();
-                    break;
-                }
-            }
-        }
+        String lastTimezone = getLastTimezoneCookieValue(req.getCookies());
 
-        if (parameter == null || parameter.isEmpty()) {
-            if (lastTimezone != null) {
-                date = getDate(lastTimezone);
-            } else {
-                date = getDate("UTC");
-            }
+        String date = "";
+        String displayText = "";
+
+        if (timezoneParam == null) {
+            date = getDate("UTC");
+            displayText = "UTC";
+        } else if ("UTC+2".equals(timezoneParam)) {
+            date = getDate("UTC+2");
+            displayText = "UTC+2";
+            resp.addCookie(new Cookie("lastTimezone", timezoneParam));
+        } else if (lastTimezone != null) {
+            date = getDate(lastTimezone);
+            displayText = lastTimezone;
         } else {
-            String encodedParameter = URLEncoder.encode(parameter, "UTF-8");
-            resp.addCookie(new Cookie("lastTimezone", parameter));
-            if (lastTimezone != null) {
-                date = getDate(parameter);
-            } else {
-                date = getDate("UTC");
-            }
+            date = getDate("UTC");
+            displayText = "UTC";
         }
 
-
-        Context context = new Context(req.getLocale(), Map.of("date", date));
+        Context context = new Context(req.getLocale(), Map.of("date", date, "displayText", displayText));
         engine.process("result", context, resp.getWriter());
+    }
+
+    private String getLastTimezoneCookieValue(Cookie[] cookies) {
+        return Arrays.stream(cookies)
+                .filter(cookie -> "lastTimezone".equals(cookie.getName()))
+                .map(Cookie::getValue)
+                .findFirst()
+                .orElse(null);
     }
 
     public String getDate(String param) {
         ZonedDateTime actualDateTime;
-        if (param == null) {
+        if ("UTC+2".equals(param)) {
+            actualDateTime = ZonedDateTime.now(ZoneId.of("UTC")).plusHours(2);
+        } else if (param == null) {
             actualDateTime = ZonedDateTime.now(ZoneId.of("UTC"));
         } else {
             actualDateTime = ZonedDateTime.now(ZoneId.of(param));
